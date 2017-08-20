f616203851d42c9bcaeff9b330be5950244d677e
streamheart <sumadi87@yahoo.com>
2017-08-20 08:26:47 +0300

    fixed imports

--- AuctionTools/src/main/java/com/stc/AuctionTools/services/UserService.java
+++ AuctionTools/src/main/java/com/stc/AuctionTools/services/UserService.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
