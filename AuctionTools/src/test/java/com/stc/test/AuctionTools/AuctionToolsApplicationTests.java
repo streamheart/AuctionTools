6cdd4a19776df3c15467173675a371837ce03852
streamheart <sumadi87@yahoo.com>
2017-08-20 13:36:43 +0300

    fixed naming

--- AuctionTools/src/test/java/com/stc/test/AuctionTools/AuctionToolsApplicationTests.java
+++ AuctionTools/src/test/java/com/stc/test/AuctionTools/AuctionToolsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stc.deleteUser;
+package com.stc.test.AuctionTools;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DeleteUserApplicationTests {
+public class AuctionToolsApplicationTests {
 
 	@Test
 	public void contextLoads() {
